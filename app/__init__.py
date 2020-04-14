6510920eb5e941b3ecfcf06d81d55b4d72faec52
rowatk <rowaneatkinson@gmail.com>
2020-04-14 03:32:19 -0500

    reconfigured production db uri

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,8 @@ SECRET_KEY = os.urandom(24)
 PROFILE_PICTURES = "./app/static/ppictures"
 
 #SQLALCHEMY DB
-SQLALCHEMY_DATABASE_URI = "postgresql://postgresr:postgresr@localhost/info_project1"
+# SQLALCHEMY_DATABASE_URI = "postgresql://postgresr:postgresr@localhost/info_project1"
+SQLALCHEMY_DATABASE_URI = "postgres://unwpgmakjxuayv:c806d6052253293a980a7fa2b2d143095850ac94241e27075e681f0a514b6849@ec2-52-7-39-178.compute-1.amazonaws.com:5432/d81nclv042osql"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
